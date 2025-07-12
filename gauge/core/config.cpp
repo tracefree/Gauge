37d213f3d5e3e79eabe7624f718ca4040f2b934a
Rie <riedev@proton.me>
2025-07-12 15:32:48 +0200

    Pinned vk-bootstrap to version v1.4.320

--- gauge/core/config.cpp
+++ gauge/core/config.cpp
@@ -4,9 +4,12 @@
 using namespace Gauge;
 
 ProjectSettings Gauge::load_project_settings(const std::string p_path) {
+    /*
     YAML::Node config = YAML::LoadFile(p_path);
     return ProjectSettings {
         .name = config["name"].as<std::string>(),
         .description =config["description"].as<std::string>()
     };
+    */
+    return ProjectSettings {};
 }
